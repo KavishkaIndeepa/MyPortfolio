12648eab2f52d64a9183f14a0874be110be9cf58
Kavishka Indeepa <101946925+KavishkaIndeepa@users.noreply.github.com>
2023-06-14 00:30:28 +0530

    Updare InMemoryPOS

--- assignment/JS/InMemoryPOS/SPA/js/Customer.js
+++ assignment/JS/InMemoryPOS/SPA/js/Customer.js
@@ -1,84 +1,133 @@
-// document.getElementById("add").addEventListener("click",function(){
-//     let id=document.getElementById("inputCId").value;
-//     let name=document.getElementById("Cname").value;
-//     let address=document.getElementById("inputAddress").value;
-//     let date=document.getElementById("Cdate").value;
+// // document.getElementById("add").addEventListener("click",function(){
+// //     let id=document.getElementById("inputCId").value;
+// //     let name=document.getElementById("Cname").value;
+// //     let address=document.getElementById("inputAddress").value;
+// //     let date=document.getElementById("Cdate").value;
+// //     console.log(id,name,address,date);
+//
+//
+// var customerArray =[];
+//
+//
+// $("#add").click(function () {
+//     let id = $("#inputCId").val();
+//     let name = $("#Cname").val();
+//     let address = $("#inputAddress").val();
+//     let date=$("#Cdate").val();
+//
 //     console.log(id,name,address,date);
-
-
-var customerArray =[];
-
+//
+//      let cus = {
+//          id: this.id,
+//          name: this.name ,
+//          address:this.address,
+//          date:this.date,
+//      }
+//     customerArray.push(cus);
+//
+//     // let tBody=document.getElementById("tblCustomer");
+// let tBody = $("#tblCustomer")
+//
+//
+//     // let tr=document.createElement("tr");
+//     // let col1= document.createElement("td");
+//     // let col2=document.createElement("td");
+//     // let col3=document.createElement("td");
+//     // let col4=document.createElement("td");
+//
+//     let tr = $('<tr>');
+//     let td1 = $('<td>');
+//     let td2 = $('<td>');
+//     let td3 = $('<td>');
+//     let td4 = $('<td>');
+//
+//
+//
+//
+//
+//     // col1.textContent=id;
+//     // col2.textContent=name;
+//     // col3.textContent=address;
+//     // col4.textContent=date;
+//
+//
+//     // td1.text(id);
+//     // td2.text(name);
+//     // td3.text(address);
+//     // td4.text(date);
+//
+//     td1.text(cus[0]);
+//     td2.text(cus[1]);
+//     td3.text(cus[2]);
+//     td4.text(cus[3]);
+//
+//
+//
+//     // tr.appendChild(col1);
+//     // tr.appendChild(col2);
+//     // tr.appendChild(col3);
+//     // tr.appendChild(col4);
+//     //
+//     //
+//     // tBody.appendChild(tr);
+//     tr.append(td1);
+//     tr.append(td2);
+//     tr.append(td3);
+//     tr.append(td4);
+//
+//     tBody.append(tr);
+//
+//
+//
+//
+//
+// });
+var customerDB = [];
 
 $("#add").click(function () {
-    let id = $("#inputCId").val();
-    let name = $("#Cname").val();
-    let address = $("#inputAddress").val();
-    let date=$("#Cdate").val();
-
-    console.log(id,name,address,date);
-
-     let cus = {
-         id: this.id,
-         name: this.name ,
-         address:this.address,
-         date:this.date,
-     }
-    customerArray.push(cus);
-
-    // let tBody=document.getElementById("tblCustomer");
-let tBody = $("#tblCustomer")
 
 
-    // let tr=document.createElement("tr");
-    // let col1= document.createElement("td");
-    // let col2=document.createElement("td");
-    // let col3=document.createElement("td");
-    // let col4=document.createElement("td");
+    let customerID = $("#inputCId").val();
+    let customerName = $("#Cname").val();
+    let customerAddress = $("#inputAddress").val();
+    let customerBDay = $("#Cdate").val();
 
-    let tr = $('<tr>');
-    let td1 = $('<td>');
-    let td2 = $('<td>');
-    let td3 = $('<td>');
-    let td4 = $('<td>');
+
+    let customerOb = {
+        id: customerID,
+        name: customerName,
+        address: customerAddress,
+        date: customerBDay
+    }
+
+
+    customerDB.push(customerOb);
 
 
 
+});
 
 
-    // col1.textContent=id;
-    // col2.textContent=name;
-    // col3.textContent=address;
-    // col4.textContent=date;
+$("#getAll").click(function () {
 
 
-    // td1.text(id);
-    // td2.text(name);
-    // td3.text(address);
-    // td4.text(date);
+    $("#tblCustomer").empty();
 
-    td1.text(cus[0]);
-    td2.text(cus[1]);
-    td3.text(cus[2]);
-    td4.text(cus[3]);
+    //get all customers
+    for (let i = 0; i < customerDB.length; i++) {
+        let id = customerDB[i].id;
+        let name = customerDB[i].name;
+        let address = customerDB[i].address;
+        let date = customerDB[i].date;
 
-
-
-    // tr.appendChild(col1);
-    // tr.appendChild(col2);
-    // tr.appendChild(col3);
-    // tr.appendChild(col4);
-    //
-    //
-    // tBody.appendChild(tr);
-    tr.append(td1);
-    tr.append(td2);
-    tr.append(td3);
-    tr.append(td4);
-
-    tBody.append(tr);
+        let row = `<tr>
+                     <td>${id}</td>
+                     <td>${name}</td>
+                     <td>${address}</td>
+                     <td>${date}</td>
+                    </tr>`;
 
 
-
-
-
+        $("#tblCustomer").append(row);
+    }
 });
--- assignment/JS/InMemoryPOS/SPA/js/Order.js
+++ assignment/JS/InMemoryPOS/SPA/js/Order.js
@@ -1,68 +1,123 @@
-// document.getElementById("orderAdd").addEventListener("click",function(){
-//     let OrderId=document.getElementById("inputId").value;
-//     let OrderCode=document.getElementById("Ocode").value;
-//     let date=document.getElementById("inputdate").value;
-//     let price=document.getElementById("price").value;
-//     let Qty=document.getElementById("OQty").value;
+// // document.getElementById("orderAdd").addEventListener("click",function(){
+// //     let OrderId=document.getElementById("inputId").value;
+// //     let OrderCode=document.getElementById("Ocode").value;
+// //     let date=document.getElementById("inputdate").value;
+// //     let price=document.getElementById("price").value;
+// //     let Qty=document.getElementById("OQty").value;
+// //     console.log(OrderId,OrderCode,date,price,Qty);
+//
+// $("#orderAdd").click(function () {
+//     let OrderId = $("#inputId").val();
+//     let OrderCode = $("#Ocode").val();
+//     let date = $("#inputdate").val();
+//     let price=$("#price").val();
+//     let Qty=$("#OQty").val();
+//
 //     console.log(OrderId,OrderCode,date,price,Qty);
+//
+//
+//
+//     // let tBody=document.getElementById("tblOrder");
+//
+//     let tBody = $("#tblOrder")
+//
+//     // let tr=document.createElement("tr");
+//     // let col1= document.createElement("td");
+//     // let col2=document.createElement("td");
+//     // let col3=document.createElement("td");
+//     // let col4=document.createElement("td");
+//     // let col5=document.createElement("td");
+//
+//     let tr = $('<tr>');
+//     let td1 = $('<td>');
+//     let td2 = $('<td>');
+//     let td3 = $('<td>');
+//     let td4 = $('<td>');
+//     let td5 = $('<td>');
+//
+//
+//
+//     // col1.textContent=OrderId;
+//     // col2.textContent=OrderCode;
+//     // col3.textContent=date;
+//     // col4.textContent=price;
+//     // col5.textContent=Qty;
+//
+//     td1.text(OrderId);
+//     td2.text(OrderCode);
+//     td3.text(date);
+//     td4.text(price);
+//     td5.text(Qty);
+//
+//     // tr.appendChild(col1);
+//     // tr.appendChild(col2);
+//     // tr.appendChild(col3);
+//     // tr.appendChild(col4);
+//     // tr.appendChild(col5);
+//     //
+//     //
+//     // tBody.appendChild(tr);
+//
+//     tr.append(td1);
+//     tr.append(td2);
+//     tr.append(td3);
+//     tr.append(td4);
+//     tr.append(td5);
+//
+//     tBody.append(tr);
+// });
+
+
+
+var orderDB = [];
+
 
 $("#orderAdd").click(function () {
+
+
     let OrderId = $("#inputId").val();
     let OrderCode = $("#Ocode").val();
     let date = $("#inputdate").val();
     let price=$("#price").val();
     let Qty=$("#OQty").val();
 
-    console.log(OrderId,OrderCode,date,price,Qty);
+
+    let orderOb = {
+        id: OrderId,
+        code: OrderCode,
+        date: date,
+        price: price,
+        qty: Qty
+    }
+
+    orderDB.push(orderOb);
 
 
 
-    // let tBody=document.getElementById("tblOrder");
-
-    let tBody = $("#tblOrder")
-
-    // let tr=document.createElement("tr");
-    // let col1= document.createElement("td");
-    // let col2=document.createElement("td");
-    // let col3=document.createElement("td");
-    // let col4=document.createElement("td");
-    // let col5=document.createElement("td");
-
-    let tr = $('<tr>');
-    let td1 = $('<td>');
-    let td2 = $('<td>');
-    let td3 = $('<td>');
-    let td4 = $('<td>');
-    let td5 = $('<td>');
+});
 
 
+$("#getAllOrder").click(function () {
 
-    // col1.textContent=OrderId;
-    // col2.textContent=OrderCode;
-    // col3.textContent=date;
-    // col4.textContent=price;
-    // col5.textContent=Qty;
+    $("#tblOrder").empty();
 
-    td1.text(OrderId);
-    td2.text(OrderCode);
-    td3.text(date);
-    td4.text(price);
-    td5.text(Qty);
 
-    // tr.appendChild(col1);
-    // tr.appendChild(col2);
-    // tr.appendChild(col3);
-    // tr.appendChild(col4);
-    // tr.appendChild(col5);
-    //
-    //
-    // tBody.appendChild(tr);
+    for (let i = 0; i < orderDB.length; i++) {
+        let id = orderDB[i].id;
+        let code = orderDB[i].code;
+        let date = orderDB[i].date;
+        let price = orderDB[i].price;
+        let qty = orderDB[i].qty;
 
-    tr.append(td1);
-    tr.append(td2);
-    tr.append(td3);
-    tr.append(td4);
-    tr.append(td5);
+        let row = `<tr>
+                     <td>${id}</td>
+                     <td>${code}</td>
+                     <td>${date}</td>
+                     <td>${price}</td>
+                     <td>${qty}</td>
+                    </tr>`;
 
-    tBody.append(tr);
+
+        $("#tblOrder").append(row);
+    }
 });
